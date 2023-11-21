435db2cbf08260f6eeb2651e5f09cfa86501c1a4
独孤九剑 <843294669@qq.com>
2023-11-21 10:51:21 +0800

    babel.config.js 解释。

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
+/** babel.config.js是一个用于配置Babel编译工具的文件。
+  * Babel是一个广泛使用的JavaScript编译器，它可以将新版本的
+  * JavaScript代码转换为向后兼容的旧版本，以便在不同的浏览器和环境中运行。
+ **/
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
