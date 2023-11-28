3284cbaaf2a8de8212bfc97f70e46283eb95c6ec
独孤九剑 <843294669@qq.com>
2023-11-28 09:40:40 +0000

    Add comment.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App);
+// 注册指令，div 内容更新时自动翻滚滚动条。
+// 注册需要放在 createApp(App); 之后
 app.directive("scrollBottom", {
     updated(el) {
        el.scrollTo({
